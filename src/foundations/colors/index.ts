20726218362c22ab7b65c25e3bf3247de58a725d
Zak <zak.s.butcher@gmail.com>
2020-02-18 09:32:30 -0500

    Revert "Feature/color palette reactadelphia"

--- src/foundations/colors/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './color-theory';
-export * from './sass-palette';
-export * from './understanding-hex';
-export * from './creating-color';
-export * from './color-and-brand';
-export * from './brand-demo';
