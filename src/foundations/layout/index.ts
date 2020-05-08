d21f194efa83fe97a56354f592d2ff5e1b44500f
zakbutcher <zak.s.butcher@gmail.com>
2020-05-08 09:09:58 -0400

    WIP add Block component to project

--- src/foundations/layout/index.ts
+++ src/foundations/layout/index.ts
@@ -1,4 +1,5 @@
-import { Block, Box } from './box';
+import { Box } from './box';
+import { Block } from './block';
 import { Flex, Row, Column } from './flex';
 
 export { Block, Box, Flex, Row, Column };
